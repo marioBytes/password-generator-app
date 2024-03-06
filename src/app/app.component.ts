8d9596e86d1f872c4b29f6f9fdd645bf6a4c94fd
marioBytes <mario.bytes92@gmail.com>
2024-03-06 14:06:39 -0700

    Lots of functionality added!
    Instead of showing error message when no options are selected, just disabled the only selected checkbox. When another checkbox is selected, reenable it.
    
    Update the strength of a password when password length is changed
    
    WIP: Ability to generate a password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ import { InputComponent } from './input/input.component';
 import { SliderComponent } from './slider/slider.component';
 import { StrengthComponent } from './strength/strength.component';
 
+interface PasswordOptions {
+  name: string;
+  label: string;
+  checked: boolean;
+  disabled: boolean;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -31,13 +38,34 @@ export class AppComponent {
 
   password: string = '';
 
-  charLength: string = '10';
+  charLength: number = 10;
+  isValid: boolean = true;
 
-  passwordOptions = [
-    { name: 'includeUppercase', label: 'Include Uppercase Letters', checked: true },
-    { name: 'includeLowercase', label: 'Include Lowercase Letters', checked: true },
-    { name: 'includeNumbers', label: 'Include Numbers', checked: true },
-    { name: 'includeSymbols', label: 'Include Symbols', checked: false },
+  passwordOptions: PasswordOptions[] = [
+    {
+      name: 'includeUppercase',
+      label: 'Include Uppercase Letters',
+      checked: true,
+      disabled: false,
+    },
+    {
+      name: 'includeLowercase',
+      label: 'Include Lowercase Letters',
+      checked: true,
+      disabled: false,
+    },
+    {
+      name: 'includeNumbers',
+      label: 'Include Numbers',
+      checked: true,
+      disabled: false,
+    },
+    {
+      name: 'includeSymbols',
+      label: 'Include Symbols',
+      checked: false,
+      disabled: false,
+    },
   ];
 
   includeUpper: string = 'true';
@@ -45,51 +73,111 @@ export class AppComponent {
   includeNumbers: string = 'true';
   includeSymbols: string = 'false';
 
-  strength: string = 'strong';
-  strengthNum: number = 9;
-
+  strength: number = 50;
 
   generatePassword(): string {
-    this.generateNumber();
-    this.password = 'PTx1f5DaFX';
-    console.log(Array(10).fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$").map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join(''))
+    this.password = Array(this.charLength)
+      .fill(
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~!@-#$'
+      )
+      .map(function (x) {
+        return x[Math.floor(Math.random() * x.length)];
+      })
+      .join('');
 
-    if (this.strengthNum < 3) {
-      this.strength = 'too weak!';
-    } else if (this.strengthNum >= 3 && this.strengthNum <= 5) {
-      this.strength = 'weak';
-    } else if (this.strengthNum > 5 && this.strengthNum <= 8) {
-      this.strength = 'medium';
-    } else {
-      this.strength = 'strong';
+    return this.password;
+  }
+
+  getStrength(): number {
+    let passwordStrength = 0;
+
+    if (this.charLength > 0) {
+      passwordStrength += 1;
+    }
+
+    if (this.charLength > 6) {
+      passwordStrength += 1;
+    }
+
+    if (this.charLength > 9) {
+      passwordStrength += 1;
+    }
+
+    if (this.charLength > 12) {
+      passwordStrength += 1;
+    }
+
+    switch (passwordStrength) {
+      case 0:
+        this.strength = 0;
+        break;
+
+      case 1:
+        this.strength = 25;
+        break;
+
+      case 2:
+        this.strength = 50;
+        break;
+
+      case 3:
+        this.strength = 75;
+        break;
+
+      case 4:
+        this.strength = 100;
+        break;
     }
 
     return this.strength;
-  }
-
-  getStrength(): string {
-    return this.strength;
-  }
-
-  generateNumber(): number {
-    this.strengthNum = Math.floor(Math.random() * (10 - 1) + 1);
-    console.log(this.strengthNum);
-
-    return this.strengthNum;
   }
 
   copyToClipboard(): any {
     this.clipboard.copy(this.password);
   }
 
-  sliderOnChange(value: string) {
-    this.charLength = value;
+  sliderOnChange(value: any) {
+    this.charLength = Number.parseInt(value);
+    this.getStrength();
   }
 
   onCheck(value: any) {
     const { target } = value;
-    const optionIndex = this.passwordOptions.findIndex((option) => option.name === target.name);
+    const optionIndex = this.getPasswordOptionIndexByName(target.name)
 
-    this.passwordOptions[optionIndex].checked = !this.passwordOptions[optionIndex].checked;
+    this.passwordOptions[optionIndex].checked =
+      !this.passwordOptions[optionIndex].checked;
+
+    this.disableOption();
+    this.reenableOption();
+  }
+
+  private disableOption() {
+    const checkedOptions = this.getPasswordOptionsByCheckedValue(true);
+
+    if (checkedOptions.length === 1) {
+      const disableOptionName = this.passwordOptions.find((option) => option.name === checkedOptions[0].name)!.name;
+      const optionIndex = this.getPasswordOptionIndexByName(disableOptionName);
+
+      this.passwordOptions[optionIndex].disabled = true;
+    }
+  }
+
+  private reenableOption() {
+    const disabledOptions = this.passwordOptions.find((option) => option.disabled === true);
+    const moreThanOneOptionSelected = this.getPasswordOptionsByCheckedValue(true);
+
+    if (disabledOptions && moreThanOneOptionSelected.length > 1) {
+      const optionIndex = this.getPasswordOptionIndexByName(disabledOptions.name);
+      this.passwordOptions[optionIndex].disabled = false;
+    }
+  }
+
+  private getPasswordOptionsByCheckedValue(value: boolean): PasswordOptions[] {
+    return this.passwordOptions.filter((option) => option.checked === value);
+  }
+
+  private getPasswordOptionIndexByName(value: string): number {
+    return this.passwordOptions.findIndex((option) => option.name === value)
   }
 }
