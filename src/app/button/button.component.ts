b397c8d065f3f7acb505de068c0011efc90d4815
marioBytes <mario.bytes92@gmail.com>
2024-03-06 12:06:04 -0700

    Add ability to disable button

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,13 +1,15 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  @Input() disabled = false;
   @Output() onClick = new EventEmitter<any>();
 
   onClickButton(event: any) {
