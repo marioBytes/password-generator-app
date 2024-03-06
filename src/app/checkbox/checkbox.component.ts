f4a1d30e8f41d2972c8761ce0627dbf418247622
marioBytes <mario.bytes92@gmail.com>
2024-03-06 13:48:01 -0700

    Let parent handle when checkbox is disabled

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -11,6 +11,7 @@ export class CheckboxComponent {
   @Input() name: string = '';
   @Input() label: string = '';
   @Input() checked: boolean = false;
+  @Input() disabled: boolean = false;
   @Output() onChange = new EventEmitter<any>();
 
   onCheck(value: any) {
